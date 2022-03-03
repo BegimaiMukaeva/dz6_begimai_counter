d79cf12bf28220279517c716c7a3ac68b74bc457
Begimai Mukaeva <begimaimukaeva2001@gmail.com>
2022-03-03 16:39:41 +0600

    dz6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Counter extends React.Component {
+  state = {
+    count: 0,
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  onIncrease = () => {
+    this.setState((oldState) => {
+      return {
+        count: oldState.count + 1,
+      };
+    });
+  };
+  onDecrease = () => {
+    this.setState((oldState) => {
+      return {
+        count: oldState.count - 1,
+      };
+    });
+  };
+
+  UNSAFE_componentWillMount() {
+    console.log("WILL MOUNT");
+  }
+  componentDidMount() {
+    this.myMount = setInterval(() => {
+      console.log(">>>>>>> HELLO");
+    }, 2000);
+  }
+
+  UNSAFE_componentWillUpdate() {
+    console.log("WILL UPDATE");
+  }
+  componentDidUpdate() {
+    console.log("DID UPDATE");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myMount);
+  }
+
+  render() {
+    console.log("RENDER");
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <input onClick={this.onIncrease} type="button" value="Increase" />
+        <input onClick={this.onDecrease} type="button" value="Decrease" />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    showCounter: true,
+  };
+  onToggleCounter = () => {
+    this.setState((oldState) => {
+      return {
+        showCounter: !oldState.showCounter,
+      };
+    });
+  };
+  render() {
+    const content = this.state.showCounter ? <Counter /> : null;
+    return (
+      <div>
+        {content}
+        <button onClick={this.onToggleCounter}>Toggle counter</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
